4cfdd4542b1b7fcbce42837b254086d2fa4b71ea
opiumthoxa <105438468+opiumthoxa@users.noreply.github.com>
2023-06-08 10:04:05 +0300

    Add files via upload

---
+++ dal/appcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApplication3.Models;
+
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Sotrudnik> Sotrudniks { get; set; }
+    public DbSet<Uvolnenie_PSJ> Uvolnenies_PSJ { get; set; }
+    public DbSet<Uvolnenie_Rukovodstvom> Uvolnenies_Rukovodstvom { get; set; }
+    public DbSet<OtdelKadrov> OtdelsKadrov { get; set; }
+}
